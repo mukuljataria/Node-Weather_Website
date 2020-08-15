f9dc11b1c83604a35c4f7171eaa4a9a9cf17c3de
mukul jataria <mukuljataria0723@gmail.com>
2020-08-15 15:13:22 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath =path.join(__dirname, '../templates/views')
 const partialsPath =path.join(__dirname, '../templates/partials')
 
-const port = process.env.PORT || 3000
-
+// const port = process.env.PORT || 3000
+const port =process.env.PORT || 3000
 
 //Setup Handlebars engine and views location
 app.set('view engine', 'hbs')
@@ -100,5 +100,5 @@ app.get('*',(req,res) =>{
 })
 
 app.listen(port,()=>{
-	console.log('Server is up on port '+ port)
+	console.log('Server is up on port '+port)
 })
